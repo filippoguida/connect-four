3bef1f7cf7598552c296d6dda9a30a153704b511
Filippo Guida <filippo.guida@icloud.com>
2019-09-03 14:27:28 +0200

    fixed multiple animation problem on DOM ex 1

--- DOMEvents1/script.js
+++ DOMEvents1/script.js
@@ -3,12 +3,14 @@ window.onload = function() {
     var visible = false;
     document.addEventListener("mousemove", function(e) {
         //move box
+        box.parentElement.style.top = e.clientY - 50 + "px";
         box.style.top = e.clientY - 50 + "px";
+        box.parentElement.style.left = e.clientX - 50 + "px";
         box.style.left = e.clientX - 50 + "px";
         //make it visible
         if (!visible) {
+            box.parentElement.classList.add("box-pulse");
             box.classList.add("box-in");
-            box.classList.add("box-pulse");
             visible = true;
         }
     });
