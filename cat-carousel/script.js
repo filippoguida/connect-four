fd0b020889f206c8e190b700d69ebe1b6e47195c
Filippo Guida <filippo.guida@icloud.com>
2019-09-06 15:54:59 +0200

    updated kitty carousel with working dots and secure transitions

--- cat-carousel/script.js
+++ cat-carousel/script.js
@@ -1,25 +1,52 @@
 (function() {
     var current = 0,
-        timeout = 500,
-        next;
+        timeout = 5000,
+        inTransition = false,
+        timer;
+
     var elements = document.querySelectorAll(".carousel-element");
-    //set event handler
-    function nextFrame() {
-        next = (current + 1) % elements.length;
+    var dots = document.querySelectorAll(".carousel-dot");
+
+    function moveTo(next) {
+        if (typeof next == "undefined") {
+            next = (current + 1) % elements.length;
+        }
+        inTransition = true;
+
+        //transition out
         elements[current].classList.remove("onscreen");
+        dots[current].classList.remove("on");
         elements[current].classList.add("offscreen");
+        //transition in
         elements[next].classList.add("onscreen");
+        dots[next].classList.add("on");
+
         current = next;
     }
+
     //set event listeners
     for (var i = 0; i < elements.length; i++) {
+        //- for transitions
         elements[i].addEventListener("transitionend", function(e) {
             if (e.target.classList.contains("offscreen")) {
+                inTransition = false;
+                //reset transition
                 e.target.classList.remove("offscreen");
-                setTimeout(nextFrame, timeout);
+                timer = setTimeout(moveTo, timeout);
             }
         });
+        //- for dots
+        function getClickHandler(i) {
+            return function() {
+                if (i != current && !inTransition) {
+                    clearTimeout(timer);
+                    moveTo(i);
+                }
+            };
+        }
+        dots[i].addEventListener("click", getClickHandler(i));
     }
-    //start animation on first image
-    setTimeout(nextFrame, timeout);
+
+    //animation start
+    timer = setTimeout(moveTo, timeout);
 })();
