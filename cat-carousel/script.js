977205b523266a10e8e2cfe027c900182bf27913
Filippo Guida <filippo.guida@icloud.com>
2019-09-10 15:29:51 +0200

    updated kitty carousel - media queries and swipe optimized for Galaxy S5

--- cat-carousel/script.js
+++ cat-carousel/script.js
@@ -7,9 +7,11 @@
     var elements = document.querySelectorAll(".carousel-element");
     var dots = document.querySelectorAll(".carousel-dot");
 
-    function moveTo(next) {
+    function move(next) {
         if (typeof next == "undefined") {
             next = (current + 1) % elements.length;
+        } else {
+            next = next % elements.length;
         }
         inTransition = true;
 
@@ -32,15 +34,36 @@
                 inTransition = false;
                 //reset transition
                 e.target.classList.remove("offscreen");
-                timer = setTimeout(moveTo, timeout);
+                timer = setTimeout(move, timeout);
             }
         });
+        //- for touch swipe
+        var swipeDir;
+        elements[i].addEventListener("touchstart", function(e) {
+            if (!inTransition) {
+                clearTimeout(timer);
+                if (e.changedTouches[0].pageX > e.target.width / 2) {
+                    swipeDir = "right";
+                } else {
+                    swipeDir = "left";
+                }
+            }
+            e.preventDefault();
+        });
+        function getHandleSwipe(i) {
+            return function() {
+                if (swipeDir == "right" && !inTransition) {
+                    move(i + 1);
+                }
+            };
+        }
+        elements[i].addEventListener("touchend", getHandleSwipe(i));
         //- for dots
         function getClickHandler(i) {
             return function() {
                 if (i != current && !inTransition) {
                     clearTimeout(timer);
-                    moveTo(i);
+                    move(i);
                 }
             };
         }
@@ -48,5 +71,5 @@
     }
 
     //animation start
-    timer = setTimeout(moveTo, timeout);
+    timer = setTimeout(move, timeout);
 })();
