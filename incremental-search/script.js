2fb2f47751ecafc92bd6cb4ff14f75e64d3c7474
Filippo Guida <filippo.guida@icloud.com>
2019-09-12 19:40:52 +0200

    prevent parallel requests to server in incremental-search

--- incremental-search/script.js
+++ incremental-search/script.js
@@ -1,58 +1,12 @@
 (function() {
-    function getResults() {
-        $("#results").empty();
+    var ajaxTimeout;
 
-        var txt = $("#country")
-            .val()
-            .toLowerCase();
-
-        if (txt) {
-            $.ajax({
-                url: "https://flame-egg.glitch.me/",
-                data: {
-                    q: txt
-                },
-                success: function(countries) {
-                    var test = countries[0].toLowerCase();
-                    if (txt && test && test.indexOf(txt) === 0) {
-                        var limit = false;
-                        for (var i = 0; i < countries.length && !limit; i++) {
-                            $("#results").append(
-                                '<p class="result">' + countries[i] + "</p>"
-                            );
-                        }
-
-                        if ($(".result").length === 4) {
-                            limit = true; //show max 4 countries
-                        }
-
-                        if ($(".result").length === 0) {
-                            $("#results").append(
-                                '<p class="result">no countries</p>'
-                            );
-                        } else {
-                            $(".result")
-                                .on("mouseover", function(e) {
-                                    $(".result").removeClass("highlight");
-                                    $(e.target).addClass("highlight");
-                                })
-                                .on("mousedown", function(e) {
-                                    $("#country").val($(e.target).text());
-                                    $("#results").empty();
-                                });
-                        }
-                    }
-                }
-            });
-        }
-    }
-
+    //-
     $("#country")
-        .on("input", getResults)
-        .on("focus", getResults)
+        .on("input", textareaHandler)
+        .on("focus", textareaHandler)
         .on("keydown", function(e) {
             var sel = $(".result.highlight");
-
             if (e.keyCode === 40) {
                 if (sel.length === 0) {
                     $(".result")
@@ -70,11 +24,70 @@
                 $("#results").empty();
             }
         })
-        .on("click", function(e) {
-            e.stopPropagation();
+        .on("blur", function() {
+            $("#results").empty();
         });
 
-    $(window).click(function() {
+    //-
+    function textareaHandler() {
+        var txt = $("#country")
+            .val()
+            .toLowerCase();
+        if (!txt) {
+            $("#results").empty();
+            return;
+        }
+        ajaxTimeout = setTimeout(function() {
+            getResults(txt);
+        }, 250);
+    }
+
+    function getResults(txt) {
+        $.ajax({
+            url: "https://flame-egg.glitch.me/",
+            data: {
+                q: txt
+            },
+            success: function(countries) {
+                if (countries.length === 0) {
+                    $("#results").empty();
+                    return;
+                }
+                var test = countries[0].toLowerCase();
+                if (!test) {
+                    return;
+                }
+                if (test.indexOf(txt) === 0) {
+                    domOperations(countries);
+                }
+            }
+        });
+    }
+
+    function domOperations(countries) {
         $("#results").empty();
-    });
+
+        var limit = false;
+        for (var i = 0; i < countries.length && !limit; i++) {
+            $("#results").append('<p class="result">' + countries[i] + "</p>");
+        }
+
+        if ($(".result").length === 4) {
+            limit = true; //show max 4 countries
+        }
+
+        if ($(".result").length === 0) {
+            $("#results").append('<p class="result">no countries</p>');
+        } else {
+            $(".result")
+                .on("mouseover", function(e) {
+                    $(".result").removeClass("highlight");
+                    $(e.target).addClass("highlight");
+                })
+                .on("mousedown", function(e) {
+                    $("#country").val($(e.target).text());
+                    $("#results").empty();
+                });
+        }
+    }
 })();
