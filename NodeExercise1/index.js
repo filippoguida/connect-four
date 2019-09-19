b374dc2cad121d0cf2c6365c53e65a6b09bd5019
Filippo Guida <filippo.guida@icloud.com>
2019-09-19 12:22:47 +0200

    minor fix on node exercise 1

--- NodeExercise1/index.js
+++ NodeExercise1/index.js
@@ -4,12 +4,9 @@ const querystring = require("querystring");
 
 const parsedUrl = url.parse(process.argv[2]);
 
-console.log("protocol is", parsedUrl.protocol);
-console.log("The host is", parsedUrl.host);
-console.log("The hostname is", parsedUrl.hostname);
-console.log("The port is", parsedUrl.port);
-console.log("The pathname is", parsedUrl.pathname);
-console.log("The query is", parsedUrl.query);
+Object.keys(parsedUrl).forEach(key => {
+    console.log("The", key, "is", parsedUrl[key]);
+});
 
 const parsedQuery = querystring.parse(parsedUrl.query);
 if (parsedQuery) {
