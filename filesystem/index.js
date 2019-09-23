945d13045ba0563cf936f39f422d8818e607137d
Filippo Guida <filippo.guida@icloud.com>
2019-09-23 11:57:59 +0200

    fixed issues from commit f81d39a10ee08cf37b380a905a7d8ac4594a04f8

--- filesystem/index.js
+++ filesystem/index.js
@@ -28,33 +28,31 @@ function logSizes(path) {
         }
     );
 }
-// logSizes("C:/Users/Filippo Guida/Desktop/coriander-code");
+logSizes("C:/Users/Filippo Guida/Desktop/coriander-code");
 
 //- EX 2
-let dirMap = {};
 function mapSizes(path, obj) {
+    let map = obj || {};
     let files = fs.readdirSync(path, {
         withFileTypes: true
     });
-
-    if (files) {
-        for (let f of files) {
-            if (f.isFile()) {
-                let s = fs.statSync(`${path}/${f.name}`);
-                if (s) {
-                    obj[f.name] = s;
-                }
-            }
-            if (f.isDirectory()) {
-                obj[f.name] = {};
-                mapSizes(`${path}/${f.name}`, obj[f.name]);
-            }
+    for (let f of files) {
+        if (f.isFile()) {
+            let s = fs.statSync(`${path}/${f.name}`);
+            map[f.name] = s.size;
+        } else if (f.isDirectory()) {
+            map[f.name] = {};
+            mapSizes(`${path}/${f.name}`, map[f.name]);
         }
     }
+    return map;
 }
 
-mapSizes("C:/Users/Filippo Guida/Desktop/coriander-code", dirMap);
 fs.writeFileSync(
     "C:/Users/Filippo Guida/Desktop/coriander-code/filesystem/map.txt",
-    JSON.stringify(dirMap, null, 4)
+    JSON.stringify(
+        mapSizes("C:/Users/Filippo Guida/Desktop/coriander-code"),
+        null,
+        4
+    )
 );
