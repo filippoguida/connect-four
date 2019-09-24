ccec1d68744c3e42018dd718685621c0c8680ae5
Filippo Guida <filippo.guida@icloud.com>
2019-09-24 19:57:29 +0200

    fixed subfolder problem in portfolio part II

--- portfolio/Contents.js
+++ portfolio/Contents.js
@@ -1,45 +1,10 @@
 const fs = require("fs");
 const path = require("path");
+
 const contentTypes = require(`${__dirname}/contentTypes.json`);
 const contentsDir = path.normalize(`${__dirname}/projects`);
 
 let contents = mapContentsSync(contentsDir);
-function mapContentsSync(dirPath, obj) {
-    let map = obj || {};
-    fs.readdirSync(dirPath, {
-        withFileTypes: true
-    }).forEach(file => {
-        if (file.isFile()) {
-            const fileUrl = `${dirPath.replace(contentsDir, "")}/${file.name}`;
-            map[fileUrl] = {
-                path: `${dirPath}/${file.name}`,
-                contentType: getContentType(file),
-                isProjectFolder: false
-            };
-        } else if (file.isDirectory()) {
-            const fileUrl = `/${file.name}`;
-            map[fileUrl] = {
-                path: `${dirPath}/${file.name}/index.html`,
-                contentType: "text/html",
-                isProjectFolder: true,
-                projectName: file.name
-            };
-            mapContentsSync(`${dirPath}/${file.name}`, map);
-        }
-    });
-    return map;
-}
-function getContentType(file) {
-    return (
-        contentTypes[
-            path
-                .extname(file.name)
-                .toLowerCase()
-                .replace(".", "")
-        ] || "application/octet-stream"
-    );
-}
-
 const contentsWatcher = fs.watch(contentsDir);
 contentsWatcher.on("change", () => {
     console.log("changed");
@@ -69,9 +34,46 @@ exports.getListHTML = function() {
     Object.keys(contents).forEach(url => {
         let content = contents[url];
         if (content.isProjectFolder) {
-            body += `<li><a href="${url}">${content.projectName}</a></li>`;
+            body += `<li><a href="${url}/index.html">${content.projectName}</a></li>`;
         }
     });
     body += "</ul></html>";
     return body;
 };
+
+function mapContentsSync(dirPath, obj, isSubDirectory = false) {
+    let map = obj || {};
+    fs.readdirSync(dirPath, {
+        withFileTypes: true
+    }).forEach(file => {
+        if (file.isFile()) {
+            const fileUrl = `${dirPath.replace(contentsDir, "")}/${file.name}`;
+            map[fileUrl] = {
+                path: `${dirPath}/${file.name}`,
+                contentType: getContentType(file),
+                isProjectFolder: false
+            };
+        } else if (file.isDirectory() && !isSubDirectory) {
+            const fileUrl = `/${file.name}`;
+            map[fileUrl] = {
+                path: `${dirPath}/${file.name}/index.html`,
+                contentType: "text/html",
+                isProjectFolder: true,
+                projectName: file.name
+            };
+            mapContentsSync(`${dirPath}/${file.name}`, map, true);
+        }
+    });
+    return map;
+}
+
+function getContentType(file) {
+    return (
+        contentTypes[
+            path
+                .extname(file.name)
+                .toLowerCase()
+                .replace(".", "")
+        ] || "application/octet-stream"
+    );
+}
