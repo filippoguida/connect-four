9ea41337d0e3c1865cc0481e976178dc6ec171ff
Filippo Guida <filippo.guida@icloud.com>
2019-09-25 18:10:08 +0200

    added cookie and auth form to root 🍪

--- portfolio-express/httpServer.js
+++ portfolio-express/httpServer.js
@@ -1,14 +1,45 @@
 const contents = require("./Contents.js");
 const express = require("express");
+const cookieParser = require("cookie-parser");
+
 var app = express();
+app.use(cookieParser());
+app.use(express.static(__dirname + "/projects"));
+
+app.use(
+    express.urlencoded({
+        extended: false
+    })
+);
 
 app.get("/", (req, res) => {
-    contents.getListHTML(html => res.send(html));
+    if (req.cookies.auth) {
+        contents.getListHTML(html => res.send(html));
+    } else {
+        res.send(`
+            <p>Do you want some cookie? 🍪</p>
+            <form method="POST">
+                <input type="checkbox" name="auth">Yes, please!
+                <button>submit</button>
+            </form>
+        `);
+    }
 });
 
-app.use(express.static(__dirname + "/projects"));
+app.post("/", (req, res) => {
+    console.log(req.body);
+    if (req.body.auth) {
+        res.cookie("auth", "true");
+    }
+    res.redirect("/");
+});
+
 app.use((req, res, next) => {
-    contents.get(req.url, content => res.send(content));
+    if (req.cookies.auth) {
+        contents.get(req.url, content => res.send(content));
+    } else {
+        res.redirect("/");
+    }
     next();
 });
 
