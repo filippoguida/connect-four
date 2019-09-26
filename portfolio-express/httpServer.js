4b34a9700315eeb35ce75ee4b0a51d63b15bb1c4
Filippo Guida <filippo.guida@icloud.com>
2019-09-26 13:50:16 +0200

    custom made module to manage contents no longer needed. Sad.

--- portfolio-express/httpServer.js
+++ portfolio-express/httpServer.js
@@ -1,46 +1,44 @@
-const contents = require("./Contents.js");
+const fs = require("fs");
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const app = express();
 
-var app = express();
-app.use(cookieParser());
+
+//Serve contents
 app.use(express.static(__dirname + "/projects"));
 
+//Cookie
+app.use(cookieParser());
 app.use(
     express.urlencoded({
         extended: false
     })
 );
-
 app.get("/", (req, res) => {
-    if (req.cookies.auth) {
-        contents.getListHTML(html => res.send(html));
+    if (req.cookies.agreed) {
+        let body = "<ul>";
+        fs.readdir(__dirname + "/projects", f => {
+            console.log(f.name);
+            body += `<li><a href="/projects/${f.name}">${f.name}</a></li>`;
+        });
+        body += "</ul>";
+        return res.send(body);
     } else {
         res.send(`
             <p>Do you want some cookie? 🍪</p>
             <form method="POST">
-                <input type="checkbox" name="auth">Yes, please!
+                <input type="checkbox" name="agreed">Yes, please!
                 <button>submit</button>
             </form>
         `);
     }
 });
-
 app.post("/", (req, res) => {
     console.log(req.body);
-    if (req.body.auth) {
-        res.cookie("auth", "true");
+    if (req.body.agreed) {
+        res.cookie("agreed", "true");
     }
     res.redirect("/");
-});
-
-app.use((req, res, next) => {
-    if (req.cookies.auth) {
-        contents.get(req.url, content => res.send(content));
-    } else {
-        res.redirect("/");
-    }
-    next();
 });
 
 app.listen(8080, () => {
