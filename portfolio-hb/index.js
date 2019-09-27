b557e008046707b8447098eaf8b6b2e005311a8b
Filippo Guida <filippo.guida@icloud.com>
2019-09-27 16:42:10 +0200

    removing automatic content manage from portfolio-hb

--- portfolio-hb/index.js
+++ portfolio-hb/index.js
@@ -1,27 +1,5 @@
-const fs = require("fs");
 const os = require("os");
 const cluster = require("cluster");
-
-//Content manager
-const projectDir = `${__dirname}/projects`;
-function collectProjects() {
-    var projects = [];
-    fs.readdirSync(projectDir).forEach(project => {
-        projects.push({
-            title: project,
-            directory: `projects/${project}`
-        });
-    });
-    fs.writeFileSync(
-        `${__dirname}/projects.json`,
-        JSON.stringify(projects, null, "\t")
-    );
-}
-
-collectProjects();
-fs.watch(projectDir).on("change", () => {
-    collectProjects();
-});
 
 //Server instances
 cluster.setupMaster({
