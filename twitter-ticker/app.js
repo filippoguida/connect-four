4e18cc9af61d07518062814a6d029ef6d9951492
Filippo Guida <filippo.guida@icloud.com>
2019-10-01 14:09:30 +0200

    Twitter API using promises and multiple feeds (part 1 and 2)

--- twitter-ticker/app.js
+++ twitter-ticker/app.js
@@ -5,24 +5,28 @@ const app = express();
 app.use(express.static(__dirname + "/ticker"));
 
 app.get("/links.json", (req, res) => {
-    twitter.getToken((err, token) => {
-        if (!err) {
-            twitter.getTweets(token, "theonion", (err, tweets) => {
-                if (!err) {
-                    res.statusCode = 200;
+    twitter
+        .getToken()
+        .then(token => {
+            Promise.all([
+                twitter.getTweets(token, "bbcworld"),
+                twitter.getTweets(token, "nytimes"),
+                twitter.getTweets(token, "forbes")
+            ])
+                .then(results => {
+                    let tweets = [];
+                    for (let result of results) {
+                        tweets.push(...result);
+                    }
+                    tweets = tweets.sort((a, b) => {
+                        return new Date(b.created_at) - new Date(a.created_at);
+                    });
+                    console.log(tweets);
                     res.json(tweets);
-                } else {
-                    console.log(err);
-                    res.statusCode = 500;
-                    res.end();
-                }
-            });
-        } else {
-            console.log(err);
-            res.statusCode = 500;
-            res.end();
-        }
-    });
+                })
+                .catch(err => res.sendCode(err));
+        })
+        .catch(err => res.sendCode(err));
 });
 
 app.listen(8080, console.log("Server listening on port 8080"));
--- twitter-ticker/twitter.js
+++ twitter-ticker/twitter.js
@@ -4,53 +4,57 @@ const secret64 = Buffer.from(
     encodeURIComponent(consumerKey) + ":" + encodeURIComponent(consumerSecret)
 ).toString("base64");
 
-module.exports.getToken = callback => {
-    const req = https.request(
-        {
-            method: "POST",
-            host: "api.twitter.com",
-            path: "/oauth2/token",
-            headers: {
-                Authorization: `Basic ${secret64}`,
-                "Content-Type":
-                    "application/x-www-form-urlencoded;charset=UTF-8"
+module.exports.getToken = function() {
+    return new Promise(function(resolve, reject) {
+        const req = https.request(
+            {
+                method: "POST",
+                host: "api.twitter.com",
+                path: "/oauth2/token",
+                headers: {
+                    Authorization: `Basic ${secret64}`,
+                    "Content-Type":
+                        "application/x-www-form-urlencoded;charset=UTF-8"
+                }
+            },
+            res => {
+                let body = "";
+                res.on("data", chunk => (body += chunk));
+                res.on("end", () => {
+                    if (res.statusCode !== 200) reject(res.statusCode);
+                    else resolve(JSON.parse(body));
+                });
             }
-        },
-        res => {
-            let body = "";
-            res.on("data", chunk => (body += chunk));
-            res.on("end", () => {
-                if (res.statusCode !== 200) callback(res.statusCode);
-                else callback(null, JSON.parse(body));
-            });
-        }
-    );
-    req.end("grant_type=client_credentials");
+        );
+        req.end("grant_type=client_credentials");
+    });
 };
 
-module.exports.getTweets = (token, user_id, callback) => {
-    const req = https.request(
-        {
-            method: "GET",
-            host: "api.twitter.com",
-            path:
-                "/1.1/statuses/user_timeline.json?count=100&screen_name=" +
-                user_id,
-            headers: {
-                Authorization: `Bearer ${token.access_token}`
+module.exports.getTweets = (token, user_id) => {
+    return new Promise(function(resolve, reject) {
+        const req = https.request(
+            {
+                method: "GET",
+                host: "api.twitter.com",
+                path:
+                    "/1.1/statuses/user_timeline.json?count=100&screen_name=" +
+                    user_id,
+                headers: {
+                    Authorization: `Bearer ${token.access_token}`
+                }
+            },
+            res => {
+                let body = "";
+                res.on("data", chunk => (body += chunk));
+                res.on("end", () => {
+                    let tweets = JSON.parse(body);
+                    if (res.statusCode !== 200) reject(res.statusCode);
+                    else resolve(filterTweets(tweets));
+                });
             }
-        },
-        res => {
-            let body = "";
-            res.on("data", chunk => (body += chunk));
-            res.on("end", () => {
-                let tweets = JSON.parse(body);
-                if (res.statusCode !== 200) callback(res);
-                else callback(null, filterTweets(tweets));
-            });
-        }
-    );
-    req.end();
+        );
+        req.end();
+    });
 };
 
 function filterTweets(tweets) {
