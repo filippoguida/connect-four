488dbbf762a22dc7d61604a44f466bc10f8454ca
Filippo Guida <filippo.guida@icloud.com>
2019-10-02 16:48:43 +0200

    jest exercises

--- testing/exercises/2/albums.test.js
+++ testing/exercises/2/albums.test.js
@@ -1,12 +1,22 @@
-const {getAlbumNames} = require('./albums');
-const spotify = require('./spotify');
+const { getAlbumNames } = require("./albums");
+const spotify = require("./spotify");
 
-jest.mock('./spotify')
+jest.mock("./spotify");
 
-test('album names are in alphabetical order', () => {
-    return getAlbumNames('meat loaf').then(
-        albumNames => {
-            expect(albumNames).toEqual(albumNames.sort());
+test("album names are in alphabetical order", () => {
+    spotify.search.mockResolvedValue({
+        albums: {
+            items: [
+                "anarchy in the BBQ",
+                "cow on a stick",
+                "eat m eat",
+                "hot wings",
+                "veggy blood",
+                "zebra with fries"
+            ]
         }
-    );
+    });
+    return getAlbumNames("meat loaf").then(albumNames => {
+        expect(albumNames).toEqual(albumNames.sort());
+    });
 });
--- testing/exercises/3/fn.js
+++ testing/exercises/3/fn.js
@@ -1,2 +1,12 @@
-module.exports = function fn() {
+module.exports = function fn(a) {
+    if (typeof a === "string") {
+        return a
+            .split("")
+            .reverse()
+            .join("");
+    } else if (Array.isArray(a)) {
+        a.forEach((r, i, a) => (a[i] = fn(r)));
+        return a;
+    }
+    return null;
 };
